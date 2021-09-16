b330c3411ab9c84b0d1b7093fc6a072b3617d10f
Darshantejha <88922513+Darshantejha@users.noreply.github.com>
2021-09-17 01:21:48 +0530

    Add files via upload

---
+++ public/script-home.js
@@ -0,0 +1,12 @@
+let x= document.getElementById("navlink");
+let y= document.getElementById("menu");
+
+function hidemenu(){
+    x.style.right="-200px";
+    //x.style.transition="1s";
+}
+
+function showmenu(){
+    x.style.right="0px";
+    //x.style.transition="1s";
+}
